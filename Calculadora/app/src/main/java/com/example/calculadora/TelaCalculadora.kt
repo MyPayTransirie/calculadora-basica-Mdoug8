6976ee58d398bea0b3877cc6959a467838b2b648
Matheus Douglas <mdra.eng18@uea.edu.br>
2020-09-10 13:02:00 -0400

    Layout da da calculadora pronta

---
+++ Calculadora/app/src/main/java/com/example/calculadora/TelaCalculadora.kt
@@ -0,0 +1,11 @@
+package com.example.calculadora
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TelaCalculadora : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_calculadora)
+    }
+}
