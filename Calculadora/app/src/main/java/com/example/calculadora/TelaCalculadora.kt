6e4f57bf87d2dd4222afebfafd2c72df0e1cdb3e
Matheus Douglas <mdra.eng18@uea.edu.br>
2020-09-10 18:19:44 -0400

    2 versao da calculadora feita

--- Calculadora/app/src/main/java/com/example/calculadora/TelaCalculadora.kt
+++ Calculadora/app/src/main/java/com/example/calculadora/TelaCalculadora.kt
@@ -2,10 +2,79 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import kotlinx.android.synthetic.main.activity_tela_calculadora.*
 
 class TelaCalculadora : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tela_calculadora)
+
+
+        btn_0.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_0.text}"
+
+        }
+        btn_1.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_1.text}"
+
+        }
+        btn_2.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_2.text}"
+
+        }
+        btn_3.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_3.text}"
+
+        }
+        btn_4.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_4.text}"
+        }
+        btn_5.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_5.text}"
+
+        }
+        btn_6.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_6.text}"
+
+        }
+        btn_7.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_7.text}"
+        }
+        btn_8.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_8.text}"
+        }
+        btn_9.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_9.text}"
+        }
+
+        btn_divisao.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_divisao.text}"
+        }
+
+        btn_multiplicacao.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_multiplicacao.text}"
+        }
+        btn_soma.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_soma.text}"
+        }
+        btn_subtracao.setOnClickListener{
+            inputValor.text= "${inputValor.text}${btn_subtracao.text}"
+        }
+
+        btn_calcular.setOnClickListener{
+
+        }
+        btn_delete.setOnClickListener{
+            if(btn_delete.isPressed){
+
+            }
+            else{
+                
+            }
+
+        }
+
+
     }
 }
